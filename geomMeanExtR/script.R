1ec24427adeecef33c3d7363a8fecf0c558f09cf
Roberto de la Cruz Moreno <37197805+RobertoCM@users.noreply.github.com>
2020-10-06 12:54:46 +0200

    Add files via upload

---
+++ geomMeanExtR/script.R
@@ -0,0 +1,13 @@
+#Installing and loading the EnvStats package
+packages <- c("EnvStats")
+if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+    install.packages(setdiff(packages, rownames(installed.packages())))  
+}
+library("EnvStats")
+
+
+data<-read.csv("data.txt", header = FALSE, sep = ",")
+epsilon<-1e-5
+source('~/geomMeanExtension.R')
+output<-geomMeanExtension(as.matrix(data),epsilon)
+print(paste0("The modified geometric mean is ",output[1]))
