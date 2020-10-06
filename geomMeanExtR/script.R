aceb2a381d9af06a31661f0fd172e280d5715825
Roberto de la Cruz Moreno <37197805+RobertoCM@users.noreply.github.com>
2020-10-06 20:07:43 +0200

    Update script.R

--- geomMeanExtR/script.R
+++ geomMeanExtR/script.R
@@ -8,6 +8,6 @@ library("EnvStats")
 
 data<-read.csv("data.txt", header = FALSE, sep = ",")
 epsilon<-1e-5
-source('~/geomMeanExtension.R')
+source('./geomMeanExtension.R')
 output<-geomMeanExtension(as.matrix(data),epsilon)
 print(paste0("The modified geometric mean is ",output[1]))
